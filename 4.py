78ced8ff62a63ba5396ad625e0a342a92cd5c536
Hector Ramos <HectorRamos.Junior@gmail.com>
2016-01-10 02:19:17 -0500

    Problems 1-4

---
+++ 4.py
@@ -0,0 +1,21 @@
+"""
+Advent of Code Day 4
+Hector Ramos
+1/10/2016
+"""
+import hashlib
+
+def firstFiveZeroHash(key):
+    counter = 0
+
+    while True:
+        h = hashlib.md5()
+        h.update(key + str(counter))
+        hashResult = h.hexdigest()
+
+        if hashResult.startswith("000000") == True:
+            return "The lowest number is %d" %counter
+
+        counter += 1
+
+print firstFiveZeroHash("iwrupvqb")
